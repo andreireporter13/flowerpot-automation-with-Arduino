5f851db50df8ce91f14174403a6557220efd6891
andreireporter13 <andrei.reporter13@gmail.com>
2023-05-28 21:46:04 +0300

    update file

--- flowerpot_automation.ino
+++ flowerpot_automation.ino
@@ -19,25 +19,44 @@
 
 #include <DHT.h>
 
+// air: humidity and temeprature
 #define DHTPIN 2          // pinul digital la care e conectat senzorul
 #define DHTTYPE DHT11     // tipul de senzor utilizat (pot fi DHT11, DHT21 sau DHT22)
+
+// earth humidity
+#define EARTH_SENSOR A0
+
+// 
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  
+  pinMode(EARTH_SENSOR, INPUT);
+  
   Serial.begin(9600);
   dht.begin();
 }
 
 void loop() {
+
+  // air: humidity and temperature senzor Value
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
+  // value for earth humidity senzor
+  int senzor_EARTH = analogRead(EARTH_SENSOR)
+
+  // Temperature and humidity - air
   Serial.print("Temperatura: ");
   Serial.print(temperature);
   Serial.print("°C | Umiditate: ");
   Serial.print(humidity);
   Serial.println("%");
+
+  // earth sensor value
+  Serial.print("Valoare senzor: ");
+  Serial.println(sensorValue);
 
   delay(2000); // așteptăm 2 secunde până la o nouă citire
 }
